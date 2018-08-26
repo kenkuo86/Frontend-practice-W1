84ff0dfffc29013ae9c4d357b8426f4309ccc422
kenkuo86 <kenkuo860129@gmail.com>
2018-08-26 16:28:05 +0800

    1

--- Q2.js
+++ Q2.js
@@ -1,1 +1,13 @@
-var n;
+function upperCase(word) {
+	let firstchar = word.charAt(0);
+	let rest = word.slice(1);
+	let re = /^[a-z]+$/;
+	if (re.test(firstchar)) {
+		firstchar = firstchar.toUpperCase();
+		return firstchar + rest;
+	} else {
+		return word;
+	}
+}
+
+console.log(upperCase("ken"));
